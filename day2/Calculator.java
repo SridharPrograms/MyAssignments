2d94942525400b42c4b3e8283d1d182110ee1ebc
SridharPrograms <117548067+SridharPrograms@users.noreply.github.com>
2022-11-06 17:33:57 -0500

    Add files via upload

---
+++ day2/Calculator.java
@@ -0,0 +1,30 @@
+package week1.day2;
+
+public class Calculator {
+	//Addition
+	public int sum(int num1,int num2,int num3) {
+		int addition=num1+num2+num3;
+		return addition;
+	}
+	//subtraction
+	public int sub(int num1,int num2) {
+		int subtraction=num1-num2;
+		return subtraction;
+	}
+	//Multiplication
+	public double mul(double num1,double num2) {
+		double multiply=num1*num2;
+		return multiply;
+	}	
+	//Division
+	public float div(float num1,float num2) {
+		float division=num1/num2;
+		return division;
+	}	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ day2/ConvertANegativeNumberToPositiveNumber.java
@@ -0,0 +1,11 @@
+package week1.day2;
+
+public class ConvertANegativeNumberToPositiveNumber {
+
+	public static void main(String[] args) {
+	int number=-650000;
+	if (number<0) {
+		System.out.println("The converted positive number is:" + -(number));
+	}
+	}
+}
